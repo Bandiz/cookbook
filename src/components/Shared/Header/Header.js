e04bd7fad78606d7384fdbbbe44472b2f8afc58f
Linas Jakseboga <linas.jakseboga@gmail.com>
2020-05-26 18:17:28 +0200

    removed logo from the header

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from '../../../logo.svg';
-import Button from 'react-bootstrap/Button';
 import HeaderNav from './HeaderNav/HeaderNav';
 
 function Header() {
@@ -8,20 +6,6 @@ function Header() {
     return (
         <header className="App-header">
             <HeaderNav />
-            <Button variant="primary">Primary</Button>
-            
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
         </header>
     )
 }
