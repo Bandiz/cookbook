299f484f9dd01260894243f0950e7e56b465b708
Laura J <laura.jakseboge@gmail.com>
2021-08-24 17:23:16 +0200

    adjusted submenu component, added styles;

--- src/components/Shared/Header/Submenu/index.tsx
+++ src/components/Shared/Header/Submenu/index.tsx
@@ -1,62 +1,31 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
-import { ListItemIcon, ListItemText, Menu, MenuItem } from "@material-ui/core";
-import useStyles from "./styles";
 import { SubmenuProps } from "../types";
 
-export default function Submenu({
-  open,
-  handleClose,
-  menuLinks,
-}: SubmenuProps) {
-  const classes = useStyles();
+import { ListItemIcon, ListItemText, MenuItem } from "@material-ui/core";
+import "./Submenu.scss";
 
+export default function Submenu({ handleClose, menuLinks }: SubmenuProps) {
   return (
-    <Menu
-      anchorEl={open}
-      keepMounted
-      open={Boolean(open)}
-      onClose={handleClose}
-      getContentAnchorEl={null}
-      anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "center",
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "center",
-      }}
-    >
-      <div className={classes.submenu}>
-        <MenuItem
-          className={classes.item}
-          onClick={handleClose}
-          component={Link}
-          to={`/recipes`}
-        >
-          Show All
-        </MenuItem>
-        {menuLinks
-          .filter((recipes) => recipes.label === "Recipes")
-          .map((link) =>
-            link.sublinks?.map((sublink, index) => {
-              const { label, icon, subUrl } = sublink;
-              return (
-                <MenuItem
-                  className={classes.item}
-                  onClick={handleClose}
-                  key={index}
-                  component={Link}
-                  to={subUrl}
-                >
-                  <ListItemIcon className={classes.icon}>{icon}</ListItemIcon>
-                  <ListItemText className={classes.label} primary={label} />
-                </MenuItem>
-              );
-            })
-          )}
-      </div>
-    </Menu>
+    <div className="submenu">
+      {menuLinks
+        .filter((recipes) => recipes.label === "Recipes")
+        .map((link) =>
+          link.sublinks?.map((sublink, index) => {
+            const { label, icon, subUrl } = sublink;
+            return (
+              <MenuItem
+                className="item"
+                onClick={handleClose}
+                key={index}
+                component={Link}
+                to={subUrl}
+              >
+                <ListItemIcon className="icon">{icon}</ListItemIcon>
+                <ListItemText className="label" primary={label} />
+              </MenuItem>
+            );
+          })
+        )}
+    </div>
   );
 }
