e1ebbab43765569587d25e1c49be23e7b72bebac
Laura J <laura.jakseboge@gmail.com>
2021-08-25 11:01:40 +0200

    replaced to .tsx and .scss; adjusted content and styles;

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import "./NotFound.css";
 import { Link } from "react-router-dom";
+import "./NotFound.scss";
 
 const NotFound = () => {
   return (
-    <section className="not-found-page section">
+    <section className="not-found-page">
       <div className="not-found-container">
-        <h1>oops! it's a dead end :(</h1>
+        <h1>Woops, we're pretty sure something went horribly wrong!</h1>
         <Link to="/" className="btn btn-primary">
           back home
         </Link>
