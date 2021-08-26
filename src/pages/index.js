d324319bcae22f157f4c73e5f663798ada8e3f62
Laura J <laura.jakseboge@gmail.com>
2021-08-26 09:58:29 +0200

    moved Recipes to .tsx; cleaned up styles;

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import { Home } from "./Home";
-import Recipes from "./Recipes/Recipes";
+import Home from "./Home/Home.js";
+import Recipes from "./Recipes/index";
 import About from "./About/About";
 import Admin from "./admin/Admin";
-import NotFound from "./NotFound/NotFound";
+import NotFound from "./NotFound/index";
 import Category from "./Category/Category";
 
 export { Home, Recipes, Category, About, Admin, NotFound };
