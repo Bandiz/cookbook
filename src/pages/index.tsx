89f2f8b599290c9ed401787c51f8d94db30ef077
Laura J <laura.jakseboge@gmail.com>
2021-09-01 16:56:56 +0200

    fixed admin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Home from "./Home";
 import Recipes from "./Recipes";
 import Recipe from "./Recipe";
 import About from "./About";
-import Admin from "./admin/Admin";
+import Admin from "./Admin";
 import NotFound from "./NotFound";
 import Category from "./Category";
 
