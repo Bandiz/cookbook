b5f9de42f41cfbe6cb3cfcea1dc638f5e0671342
Laura J <laura.jakseboge@gmail.com>
2021-09-07 15:57:49 +0200

    added getCategories and getRecipies hooks

--- src/RecipesContext.tsx
+++ src/RecipesContext.tsx
@@ -1,42 +1,44 @@
 import { useState, useContext, createContext, ReactNode } from "react";
-import { _categories, _recipeById, _recipes, _token } from "./contextApi/apiUrl";
-import { Category, Recipe, UserSession } from "./types";
+
+import { Recipe, UserSession } from "./types";
+import { useAuth } from "./AuthContext";
 
 export const RecipesContext = createContext({} as RecipesContextObject);
 
 interface RecipesContextObject {
     recipes: Recipe[];
+    setRecipes: (recipes: Recipe[]) => void;
     loading: boolean;
     userData: UserSession | null;
     logout: () => void;
-    fetchRecipes: () => Promise<void>;
     fetchRecipe: (id: string) => Promise<Recipe>;
     fetchUserData: (tokenId: string) => Promise<void>;
-    categories: Category[];
-    fetchCategories: () => Promise<void>;
+    categories: string[];
+    setCategories: (category: string[]) => void;
 }
 
 export const RecipesProvider = ({ children }: { children?: ReactNode }) => {
+    const { httpClient } = useAuth();
+
     const [loading, setLoading] = useState(false);
 
     const [recipes, setRecipes] = useState<Recipe[]>([]);
 
     const [userData, setUserData] = useState<UserSession | null>(null);
 
-    const [categories, setCategories] = useState<Category[]>([]);
+    const [categories, setCategories] = useState<string[]>([]);
 
-    const fetchRecipes = async () => {
-        setLoading(true);
-        try {
-            const response = await fetch(_recipes);
-            const recipes = await response.json();
-            setRecipes(recipes);
-            setLoading(false);
-        } catch (error) {
-            console.log(error);
-            setLoading(false);
-        }
-    };
+    // const fetchRecipes = async () => {
+    //     setLoading(true);
+    //     try {
+    //         const recipes = await getRecipesRequest();
+    //         setRecipes(recipes);
+    //         setLoading(false);
+    //     } catch (error) {
+    //         console.log(error);
+    //         setLoading(false);
+    //     }
+    // };
 
     const fetchRecipeId = async (id: string) => {
         const recipe = recipes.find((x) => x.id === id);
@@ -47,7 +49,7 @@ export const RecipesProvider = ({ children }: { children?: ReactNode }) => {
 
         setLoading(true);
         try {
-            const response = await fetch(`${_recipeById}${id}`);
+            const response = await fetch(`https://localhost:44329/api/v1/Recipe/${id}`);
             const recipe = await response.json();
             setLoading(false);
             setRecipes((prev) => [...prev, recipe]);
@@ -61,31 +63,24 @@ export const RecipesProvider = ({ children }: { children?: ReactNode }) => {
     const fetchUserData = async (tokenId: string) => {
         setLoading(true);
         try {
-            const response = await fetch(`${_token}${tokenId}`);
-            const userData = (await response.json()) as UserSession;
+            const { data: userData } = await httpClient.get<UserSession>("/token", {
+                params: {
+                    t: tokenId,
+                },
+            });
             setUserData(userData);
+            httpClient.defaults.headers.authorization = `bearer ${userData.token}`;
             setLoading(false);
         } catch (error) {
             console.log(error);
-            setLoading(false);
-        }
-    };
-
-    const fetchCategories = async () => {
-        setLoading(true);
-        try {
-            const response = await fetch(_categories);
-            const categories = await response.json();
-            setCategories(categories);
-            setLoading(false);
-        } catch (error) {
-            console.log(error);
+            httpClient.defaults.headers.authorization = "";
             setLoading(false);
         }
     };
 
     const logout = () => {
         setUserData(null);
+        httpClient.defaults.headers.authorization = "";
     };
 
     console.log("context loaded");
@@ -95,12 +90,13 @@ export const RecipesProvider = ({ children }: { children?: ReactNode }) => {
             value={{
                 loading,
                 recipes,
-                fetchRecipes,
+                setRecipes,
+                // fetchRecipes,
                 fetchRecipe: fetchRecipeId,
                 userData,
                 fetchUserData,
                 categories,
-                fetchCategories,
+                setCategories,
                 logout,
             }}
         >
---
+++ src/api/getRecipes.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import { useAuth } from "../AuthContext";
+import { useGlobalContext } from "../RecipesContext";
+import { Recipe } from "../types";
+
+export function getRecipes() {
+    const [loading, setLoading] = useState(false);
+    const { httpClient } = useAuth();
+    const { setRecipes } = useGlobalContext();
+
+    const getRecipesRequest = async () => {
+        setLoading(true);
+        try {
+            const response = await httpClient.get<Recipe[]>(`/v1/Recipe`);
+            setRecipes(response.data);
+            return response.data;
+        } catch (error) {
+            console.log(error);
+            return [];
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return { getRecipesRequest, getRecipesLoading: loading };
+}
