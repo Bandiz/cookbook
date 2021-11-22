800969c81f7d6a3951a6efe22df1163d76019394
Linas Jakseboga <linas.jakseboga@gmail.com>
2021-11-22 16:32:42 +0100

    updated deleteCategory api; updated CategoryRow to include a dialog before committing a delete

--- src/api/categories/deleteCategory.ts
+++ src/api/categories/deleteCategory.ts
@@ -2,21 +2,49 @@ import { useState } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import { useGlobalContext } from '../../contexts/RecipesContext';
 
+type DeleteCategoryResponse =
+    | {
+          type: 'response';
+      }
+    | {
+          type: 'error';
+          error: string;
+      };
+
 export function DeleteCategory() {
     const [loading, setLoading] = useState(false);
     const { httpClient } = useAuth();
-    const { categories, setCategories } = useGlobalContext();
+    const { userData } = useGlobalContext();
 
-    const deleteCategoryRequest = async (categoryName: string) => {
+    const deleteCategoryRequest = async (categoryName: string): Promise<DeleteCategoryResponse> => {
+        if (!userData || !userData.token) {
+            return {
+                type: 'error',
+                error: 'Unauthorized',
+            };
+        }
         setLoading(true);
         try {
-            const response = await httpClient.delete<string>(`/v1/Category/${categoryName}`);
-            const resData = response.data;
-            setCategories(categories.filter((c) => c !== resData));
-            return resData;
-        } catch (error) {
-            console.log(error);
-            return [];
+            await httpClient.delete(`/v1/Category/${categoryName}`, {
+                headers: {
+                    Authorization: `Bearer ${userData.token}`,
+                },
+            });
+            return {
+                type: 'response',
+            };
+        } catch (e) {
+            if (e instanceof Error) {
+                return {
+                    type: 'error',
+                    error: e.message,
+                };
+            }
+            //TODO: return error types
+            return {
+                type: 'error',
+                error: 'Problem in the backend',
+            };
         } finally {
             setLoading(false);
         }
--- src/components/Tabs/CategoriesTable/CategoryRow.tsx
+++ src/components/Tabs/CategoriesTable/CategoryRow.tsx
@@ -1,7 +1,13 @@
-import { ChangeEvent, ReactNode, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import {
+    Button,
     // Box,
     Checkbox,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
     // Collapse,
     IconButton,
     // Table,
@@ -22,6 +28,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { Category } from '../../../types';
 import { UpdateCategoryVisibility } from '../../../api/categories/updateCategoryVisibility';
 import { useAdmin } from '../../../contexts/AdminContext';
+import { DeleteCategory } from '../../../api/categories/deleteCategory';
 
 interface CategoryRowProps {
     category: Category;
@@ -35,6 +42,7 @@ export function CategoryRow({ category, disabled }: CategoryRowProps) {
 
     const { categories, setCategories } = useAdmin();
     const { updateCategoryVisibilityRequest, updateCategoryVisibilityLoading } = UpdateCategoryVisibility();
+    const { deleteCategoryRequest, deleteCategoryLoading } = DeleteCategory();
 
     function handleOnVisibilityChange(event: ChangeEvent<HTMLInputElement>) {
         setVisible(event.target.checked);
@@ -56,85 +64,130 @@ export function CategoryRow({ category, disabled }: CategoryRowProps) {
         );
     }
 
-    let tableCells: ReactNode[];
+    function EditMode() {
+        return (
+            <>
+                <TableCell colSpan={2} component="th" scope="row">
+                    {category.categoryName}
+                </TableCell>
+                <TableCell colSpan={3} component="th" scope="row">
+                    <Checkbox
+                        checked={visible}
+                        onChange={handleOnVisibilityChange}
+                        inputProps={{ 'aria-label': 'is menu visible' }}
+                    />
+                </TableCell>
+                <TableCell align="right">
+                    <Tooltip title="Save">
+                        <span>
+                            <IconButton
+                                disabled={disabled || updateCategoryVisibilityLoading}
+                                onClick={handleOnSaveClick}
+                            >
+                                <SaveIcon />
+                            </IconButton>
+                        </span>
+                    </Tooltip>
+                    <Tooltip title="Cancel">
+                        <span>
+                            <IconButton
+                                disabled={disabled || updateCategoryVisibilityLoading}
+                                onClick={() => setEditMode(false)}
+                            >
+                                <CancelIcon />
+                            </IconButton>
+                        </span>
+                    </Tooltip>
+                </TableCell>
+            </>
+        );
+    }
 
-    if (!editMode) {
-        tableCells = [
-            <TableCell key={0}>
-                <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-                    {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                </IconButton>
-            </TableCell>,
-            <TableCell key={1} component="th" scope="row">
-                {category.categoryName}
-            </TableCell>,
-            <TableCell key={2} component="th" scope="row">
-                <Checkbox checked={category.visible} disabled inputProps={{ 'aria-label': 'is menu visible' }} />
-            </TableCell>,
-            <TableCell key={3} component="th" scope="row">
-                <Tooltip title={category.createdAt.format('YYYY-DD-MM HH:mm')} arrow>
-                    <span>{category.createdBy}</span>
-                </Tooltip>
-            </TableCell>,
-            <TableCell key={4} component="th" scope="row">
-                <Tooltip title={category.updatedAt.format('YYYY-DD-MM HH:mm')} arrow>
-                    <span>{category.updatedBy}</span>
-                </Tooltip>
-            </TableCell>,
-            <TableCell key={5} align="right">
-                <Tooltip title="Edit">
-                    <span>
-                        <IconButton disabled={disabled} onClick={() => setEditMode(true)}>
-                            <EditIcon />
-                        </IconButton>
-                    </span>
-                </Tooltip>
-                <Tooltip title="Delete">
-                    <span>
-                        <IconButton disabled={disabled}>
-                            <DeleteIcon />
-                        </IconButton>
-                    </span>
-                </Tooltip>
-            </TableCell>,
-        ];
-    } else {
-        tableCells = [
-            <TableCell key={0} colSpan={2} component="th" scope="row">
-                {category.categoryName}
-            </TableCell>,
-            <TableCell key={1} colSpan={3} component="th" scope="row">
-                <Checkbox
-                    checked={visible}
-                    onChange={handleOnVisibilityChange}
-                    inputProps={{ 'aria-label': 'is menu visible' }}
-                />
-            </TableCell>,
-            <TableCell key={2} align="right">
-                <Tooltip title="Save">
-                    <span>
-                        <IconButton disabled={disabled || updateCategoryVisibilityLoading} onClick={handleOnSaveClick}>
-                            <SaveIcon />
-                        </IconButton>
-                    </span>
-                </Tooltip>
-                <Tooltip title="Cancel">
-                    <span>
-                        <IconButton
-                            disabled={disabled || updateCategoryVisibilityLoading}
-                            onClick={() => setEditMode(false)}
-                        >
-                            <CancelIcon />
-                        </IconButton>
-                    </span>
-                </Tooltip>
-            </TableCell>,
-        ];
+    function DisplayMode() {
+        const [deleteOpen, setDeleteOpen] = useState(false);
+
+        function handleOnDialogClose() {
+            setDeleteOpen(false);
+        }
+
+        async function handleOnConfirmDeletion() {
+            handleOnDialogClose();
+            await deleteCategoryRequest(category.categoryName);
+            setCategories(categories.filter((x) => x.categoryName !== category.categoryName));
+        }
+
+        return (
+            <>
+                <TableCell>
+                    <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    {category.categoryName}
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    <Checkbox checked={category.visible} disabled inputProps={{ 'aria-label': 'is menu visible' }} />
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    <Tooltip title={category.createdAt.format('YYYY-DD-MM HH:mm')} arrow>
+                        <span>{category.createdBy}</span>
+                    </Tooltip>
+                </TableCell>
+                <TableCell component="th" scope="row">
+                    <Tooltip title={category.updatedAt.format('YYYY-DD-MM HH:mm')} arrow>
+                        <span>{category.updatedBy}</span>
+                    </Tooltip>
+                </TableCell>
+                <TableCell align="right">
+                    <Tooltip title="Edit">
+                        <span>
+                            <IconButton disabled={disabled || deleteCategoryLoading} onClick={() => setEditMode(true)}>
+                                <EditIcon />
+                            </IconButton>
+                        </span>
+                    </Tooltip>
+                    <Tooltip title="Delete">
+                        <span>
+                            <IconButton
+                                disabled={disabled || deleteCategoryLoading}
+                                onClick={() => setDeleteOpen(true)}
+                            >
+                                <DeleteIcon />
+                            </IconButton>
+                        </span>
+                    </Tooltip>
+                    <Dialog
+                        open={deleteOpen}
+                        onClose={handleOnDialogClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">
+                            Do you want to delete <strong>{category.categoryName}</strong>?
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                                By deleting this category, you will remove if from all the recipes that use it.
+                            </DialogContentText>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button color="primary" onClick={handleOnDialogClose} autoFocus>
+                                Cancel
+                            </Button>
+                            <Button variant="contained" color="error" onClick={handleOnConfirmDeletion}>
+                                Delete
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </TableCell>
+            </>
+        );
     }
 
     return (
         <>
-            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>{tableCells}</TableRow>
+            <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>{editMode ? <EditMode /> : <DisplayMode />}</TableRow>
             {/* <TableRow>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
                     <Collapse in={open} timeout="auto" unmountOnExit>
