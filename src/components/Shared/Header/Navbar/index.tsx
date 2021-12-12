ceae2ae91eb36eda413006f0eaf8e84c3160f5f2
Linas Jakseboga <linas.jakseboga@gmail.com>
2021-12-12 12:38:46 +0200

    added navbar login/account menu logic

--- src/components/Shared/Header/Navbar/index.tsx
+++ src/components/Shared/Header/Navbar/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 import { GiHamburger, GiSteak, GiFruitBowl, GiCookingPot } from 'react-icons/gi';
 import { CgBowl } from 'react-icons/cg';
-import { ListItemIcon, ListItemText, Menu, MenuItem } from '@mui/material';
+import { Avatar, Box, Divider, IconButton, ListItemIcon, ListItemText, Menu, MenuItem, Tooltip } from '@mui/material';
+import LoginIcon from '@mui/icons-material/Login';
+import Logout from '@mui/icons-material/Logout';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 
 import logo from '../Logo/icon.png';
 import './HeaderNav.scss';
@@ -12,14 +15,16 @@ import Hamburger from '../Hamburger';
 import { useRecipes } from '../../../../contexts/RecipesContext';
 import { ABOUT, ADMIN, HOME, RECIPES, LOGIN } from '../../../../constants/routes';
 import { useAuth } from '../../../../contexts/AuthContext';
+import { User } from '../../../../types';
 
 const Navbar = () => {
     // const { userData } = useGlobalContext();
     const [openSubmenu, setOpenSubmenu] = useState<null | HTMLElement>(null);
     const [openHamburger, setOpenHamburger] = useState(false);
+    const navigate = useNavigate();
 
     const { categories } = useRecipes();
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, user, logout } = useAuth();
 
     const menuLinks = [
         { label: 'Home', url: HOME, sublinks: [] },
@@ -56,6 +61,43 @@ const Navbar = () => {
         setOpenHamburger(false);
         handleCloseSubmenu();
     };
+
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    function stringToColor(string: string) {
+        let hash = 0;
+        let i;
+
+        for (i = 0; i < string.length; i += 1) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        let color = '#';
+
+        for (i = 0; i < 3; i += 1) {
+            const value = (hash >> (i * 8)) & 0xff;
+            color += `00${value.toString(16)}`.substring(-2);
+        }
+
+        return color;
+    }
+
+    function stringAvatar(user: User) {
+        const { name, lastName } = user;
+        return {
+            sx: {
+                bgcolor: stringToColor(name),
+            },
+            children: `${name[0]}${lastName[0]}`,
+        };
+    }
 
     return (
         <>
@@ -119,14 +161,84 @@ const Navbar = () => {
                             About
                         </Link>
                         {!isAuthenticated && (
-                            <Link to={LOGIN} className="link-btn">
+                            <Box
+                                to={LOGIN}
+                                component={Link}
+                                className="link-btn"
+                                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+                            >
+                                <LoginIcon sx={{ mr: 1 }} />
                                 Login
-                            </Link>
+                            </Box>
                         )}
-                        {isAuthenticated && (
-                            <Link to={ADMIN} className="link-btn">
-                                Admin
-                            </Link>
+                        {isAuthenticated && user && (
+                            <>
+                                <Tooltip title="Account">
+                                    <IconButton onClick={handleClick} size="small" sx={{ ml: 2 }}>
+                                        <Avatar {...stringAvatar(user)} sx={{ width: 32, height: 32 }}></Avatar>
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    anchorEl={anchorEl}
+                                    open={open}
+                                    onClose={handleClose}
+                                    onClick={handleClose}
+                                    PaperProps={{
+                                        elevation: 0,
+                                        sx: {
+                                            overflow: 'visible',
+                                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                            mt: 1.5,
+                                            '& .MuiAvatar-root': {
+                                                width: 32,
+                                                height: 32,
+                                                ml: -0.5,
+                                                mr: 1,
+                                            },
+                                            '&:before': {
+                                                content: '""',
+                                                display: 'block',
+                                                position: 'absolute',
+                                                top: 0,
+                                                right: 14,
+                                                width: 10,
+                                                height: 10,
+                                                bgcolor: 'background.paper',
+                                                transform: 'translateY(-50%) rotate(45deg)',
+                                                zIndex: 0,
+                                            },
+                                        },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem>
+                                        <Avatar /> Profile
+                                    </MenuItem>
+                                    <Divider />
+                                    {user.isAdmin && (
+                                        <MenuItem
+                                            onClick={() => {
+                                                navigate(ADMIN);
+                                            }}
+                                        >
+                                            <ListItemIcon>
+                                                <AdminPanelSettingsIcon fontSize="small" />
+                                            </ListItemIcon>
+                                            Admin
+                                        </MenuItem>
+                                    )}
+                                    <MenuItem onClick={logout}>
+                                        <ListItemIcon>
+                                            <Logout fontSize="small" />
+                                        </ListItemIcon>
+                                        Logout
+                                    </MenuItem>
+                                </Menu>
+                            </>
+                            // <Link to={ADMIN} className="link-btn">
+                            //     Admin
+                            // </Link>
                         )}
                     </div>
                 </div>
