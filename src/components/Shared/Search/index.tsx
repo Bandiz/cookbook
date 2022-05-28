dea9eac63a913a80c990c21f1913a7cf2353965b
Laura J <laura.jakseboge@gmail.com>
2022-05-28 11:24:17 +0200

    renewed search component

--- src/components/Shared/Search/index.tsx
+++ src/components/Shared/Search/index.tsx
@@ -1,21 +1,55 @@
-import { InputBase } from '@mui/material';
+import { alpha, InputBase, styled } from '@mui/material';
 import SearchIcon from '@mui/icons-material//Search';
 
-import './Search.scss';
-
 export default function Search() {
+    const Search = styled('div')(({ theme }) => ({
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    }));
+
+    const SearchIconWrapper = styled('div')(({ theme }) => ({
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }));
+
+    const StyledInputBase = styled(InputBase)(({ theme }) => ({
+        color: 'inherit',
+        '& .MuiInputBase-input': {
+            padding: theme.spacing(1, 1, 1, 0),
+            // vertical padding + font size from searchIcon
+            paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('md')]: {
+                width: '12ch',
+                '&:focus': {
+                    width: '20ch',
+                },
+            },
+        },
+    }));
+
     return (
-        <div className="search">
-            <div className="search-icon">
+        <Search>
+            <SearchIconWrapper>
                 <SearchIcon />
-            </div>
-            <InputBase
-                placeholder="Search…"
-                classes={{
-                    root: 'inputRoot',
-                }}
-                inputProps={{ 'aria-label': 'search' }}
-            />
-        </div>
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" inputProps={{ 'aria-label': 'search' }} />
+        </Search>
     );
 }
