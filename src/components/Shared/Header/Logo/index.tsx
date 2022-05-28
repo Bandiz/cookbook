5b247c6e72634f9e0687b4bb7308bd5f3ff728b5
Laura J <laura.jakseboge@gmail.com>
2022-05-28 11:26:03 +0200

    renewed logo component

--- src/components/Shared/Header/Logo/index.tsx
+++ src/components/Shared/Header/Logo/index.tsx
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom';
 import { HOME } from '../../../../constants/routes';
+import { Box } from '@mui/material';
 
 import logo from './icon.png';
 
-export const Logo = () => {
+const Logo = () => {
     return (
-        <div className="logo">
-            <Link to={HOME}>
-                <img src={logo} alt="Logo" />
-            </Link>
-        </div>
+        <Link to={HOME}>
+            <Box
+                component="img"
+                alt="Logo"
+                src={logo}
+                sx={{
+                    width: { xs: 60, md: 80 },
+                }}
+            />
+        </Link>
     );
 };
+export default Logo;
