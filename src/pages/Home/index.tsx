654e6bbbf71e5d047e08b19cd92e3f04f8f07a4b
Laura J <laura.jakseboge@gmail.com>
2022-06-01 16:43:29 +0200

    refactored carousel and recipesPreview components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,72 +1,21 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import recipesData from '../Recipes/RecipesData.json';
-import './Home.scss';
+import { useEffect } from 'react';
+import { useRecipes } from '../../contexts/RecipesContext';
+import Carousel from './Carousel';
+import RecipesPreview from './RecipesPreview';
 
 function Home() {
-    const [index, setIndex] = useState(0);
-    const { _id, title, image, categories } = recipesData[index];
+    const { recipes, fetchRecipes } = useRecipes();
 
-    const checkNumber = (number: number) => {
-        if (number > recipesData.length - 1) {
-            return 0;
+    useEffect(() => {
+        if (recipes.length === 0) {
+            fetchRecipes();
         }
-        if (number < 0) {
-            return recipesData.length - 1;
-        }
-        return number;
-    };
-    const nextRecipe = () => {
-        setIndex((index) => {
-            let newIndex = index + 1;
-            return checkNumber(newIndex);
-        });
-    };
-    const prevRecipe = () => {
-        setIndex((index) => {
-            let newIndex = index - 1;
-            return checkNumber(newIndex);
-        });
-    };
-    // TEMPORARY COMMENTED
-    //
-    // useEffect(() => {
-    //   let slider = setInterval(() => {
-    //     setIndex((index) => {
-    //       let newIndex = index + 1;
-    //       return checkNumber(newIndex);
-    //     });
-    //   }, 5000);
-    //   return () => clearInterval(slider);
-    // }, [index]);
+    }, []);
 
     return (
-        <div>
-            <section className="slider">
-                <div className="img-container">
-                    <Link to={`/recipe/${_id}`}>
-                        <img src={image} alt={title} className="recipe-img" />
-                    </Link>
-                    <div className="slider-center">
-                        <h4 className="title">{title}</h4>
-                        <Link to={`/category/${categories}`}>
-                            <p className="category">{categories}</p>
-                        </Link>
-                        <Link to={`/recipe/${_id}`} className="btn btn-primary">
-                            read more
-                        </Link>
-                    </div>
-                    <div className="button-container">
-                        <button className="prev" onClick={prevRecipe}>
-                            <FiChevronLeft />
-                        </button>
-                        <button className="next" onClick={nextRecipe}>
-                            <FiChevronRight />
-                        </button>
-                    </div>
-                </div>
-            </section>
+        <div style={{ padding: '10px 20px' }}>
+            <Carousel />
+            <RecipesPreview />
         </div>
     );
 }
