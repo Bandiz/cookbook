84e7b54f32e6d13f8ea9fa8cc101d7e4e8cb72ed
Linas Jakseboga <linas.jakseboga@gmail.com>
2022-06-12 13:38:08 +0200

    styling changes for session hooks

--- src/api/session/index.ts
+++ src/api/session/index.ts
@@ -2,24 +2,30 @@ import { useMutation, useQuery, useQueryClient } from 'react-query';
 import httpClient, { dataGet } from '../httpClient';
 import { SessionCheck, UserSession } from './types';
 
+const sessionKey = 'session';
+
 export function useSession() {
-    return useQuery('session', dataGet<SessionCheck>('auth/v1/isLoggedIn'));
+    return useQuery(sessionKey, dataGet<SessionCheck>('auth/isLoggedIn'), {
+        refetchOnWindowFocus: false,
+        refetchIntervalInBackground: true,
+        refetchInterval: 5 * 60 * 1000,
+    });
 }
 
 export function useGoogleSessionMutation() {
     const queryClient = useQueryClient();
     return useMutation(
-        (token: string) =>
-            httpClient
-                .post<UserSession>('auth/v1/login/google', null, {
-                    params: {
-                        t: token,
-                    },
-                })
-                .then((x) => x.data),
+        async (token: string) => {
+            const response = await httpClient.post<UserSession>('auth/login/google', null, {
+                params: {
+                    t: token,
+                },
+            });
+            return response.data;
+        },
         {
             onSuccess: (data) => {
-                queryClient.setQueryData<SessionCheck>('session', { isLoggedIn: true, user: data.user });
+                queryClient.setQueryData(sessionKey, createSessionObject(data));
             },
         }
     );
@@ -29,17 +35,17 @@ export function useLoginSessionMutation() {
     const queryClient = useQueryClient();
 
     return useMutation(
-        (formData: FormData) =>
-            httpClient
-                .post<UserSession>('auth/v1/login', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data',
-                    },
-                })
-                .then((x) => x.data),
+        async (formData: FormData) => {
+            const response = await httpClient.post<UserSession>('auth/login', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data;
+        },
         {
             onSuccess: (data) => {
-                queryClient.setQueryData<SessionCheck>('session', { isLoggedIn: true, user: data.user });
+                queryClient.setQueryData(sessionKey, createSessionObject(data));
             },
         }
     );
@@ -47,9 +53,20 @@ export function useLoginSessionMutation() {
 
 export function useLogoutSessionMutation() {
     const queryClient = useQueryClient();
-    return useMutation(() => httpClient.delete('auth/v1/logout').then((x) => x.data), {
-        onSuccess: () => {
-            queryClient.invalidateQueries('session');
+    return useMutation(
+        async () => {
+            const response = await httpClient.delete('auth/logout');
+            return response.data;
         },
-    });
+        {
+            onSuccess: () => {
+                queryClient.setQueryData(sessionKey, createSessionObject());
+            },
+        }
+    );
+}
+
+function createSessionObject(data?: UserSession) {
+    const session: SessionCheck = { isLoggedIn: Boolean(data?.user), user: data?.user };
+    return session;
 }
