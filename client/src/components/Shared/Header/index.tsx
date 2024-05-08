32a9c11e590f596b2e3339e3281c7360a4e9cc26
Laura <laura@bitrefill.com>
2024-05-08 21:34:29 +0200

    new nav

--- client/src/components/Shared/Header/index.tsx
+++ client/src/components/Shared/Header/index.tsx
@@ -1,55 +1,82 @@
-import { useState } from 'react';
-import { Menu, MenuProps } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { Avatar, Menu, MenuProps } from 'antd';
 
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import { useAuth } from '../../../contexts/AuthContext';
+import { ABOUT, ADMIN, HOME, LOGIN, RECIPES } from '../../../constants/routes';
+import { useLogoutSessionMutation } from '../../../api/session';
 
 function Header() {
+    const navigate = useNavigate();
+    // const location = useLocation();
+    const logoutSessionMutation = useLogoutSessionMutation();
     const { isAuthenticated } = useAuth();
-    const [current, setCurrent] = useState('Home');
+    // const [current, setCurrent] = useState(location.pathname);
+    const { user, isAdmin } = useAuth();
 
     const onClick: MenuProps['onClick'] = (e) => {
-        console.log('click ', e);
-        setCurrent(e.key);
+        // setCurrent(e.key);
+
+        if (e.key === HOME) {
+            navigate(HOME);
+        } else if (e.key === RECIPES) {
+            navigate(RECIPES);
+        } else if (e.key === ABOUT) {
+            navigate(ABOUT);
+        } else if (e.key === LOGIN) {
+            navigate(LOGIN);
+        } else if (e.key === ADMIN) {
+            navigate(ADMIN);
+        } else if (e.key === 'logout') {
+            logoutSessionMutation.mutate();
+        }
     };
 
     return (
         <>
-            <Navbar />
+            {/* <Navbar /> */}
             <Menu
                 theme="dark"
                 mode="horizontal"
                 style={{ flex: 1, minWidth: 0 }}
-                selectedKeys={[current]}
+                // selectedKeys={[current]}
+                selectable={false}
                 onClick={onClick}
                 items={[
                     {
-                        key: '1',
+                        key: HOME,
                         label: 'Home',
                     },
                     {
-                        key: '2',
+                        key: RECIPES,
                         label: 'Recipes',
                     },
                     {
-                        key: '3',
+                        key: ABOUT,
                         label: 'About',
                     },
-                    ...(isAuthenticated
+                    ...(!isAuthenticated
                         ? [
                               {
-                                  key: '4',
+                                  key: LOGIN,
                                   label: 'Login',
-                                  children: [
-                                      { label: 'Admin', key: 'admin' },
-                                      { label: 'Logout', key: 'logout' },
-                                  ],
                               },
                           ]
                         : [
                               {
-                                  key: '4',
-                                  label: 'Login',
+                                  key: '',
+                                  label: '',
+                                  icon: (
+                                      <Avatar
+                                          size="large"
+                                          icon={user?.name.slice(0, 1).toUpperCase()}
+                                          style={{ backgroundColor: '#f56a00', verticalAlign: 'middle' }}
+                                      />
+                                  ),
+                                  children: [
+                                      ...(isAdmin ? [{ label: 'Admin', key: ADMIN }] : []),
+                                      { label: 'Logout', key: 'logout' },
+                                  ],
                               },
                           ]),
                 ]}
