0be1893be02c97432c09bf03fe0d5f0aef637235
Laura <laura@bitrefill.com>
2024-06-21 20:45:14 +0200

    removed mui, replaced carousel with antd component for home page

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,12 +1,40 @@
-import Carousel from './Carousel';
-import RecipesPreview from './RecipesPreview';
+import { Carousel, Spin } from 'antd';
+import { useRecipes } from '../../api/recipes';
 
 function Home() {
+    const { data: recipes, isLoading, isError } = useRecipes();
+    const contentStyle: React.CSSProperties = {
+        height: '400px',
+        width: '700px',
+        color: '#fff',
+        textAlign: 'center',
+        background: '#364d79',
+        objectFit: 'cover',
+        margin: '0 auto',
+    };
+
+    if (isLoading) {
+        return <Spin spinning size="large" />;
+    }
+
+    if (isError || !recipes) {
+        return null;
+    }
+
     return (
-        <div style={{ padding: '10px 20px' }}>
-            <Carousel />
-            <RecipesPreview />
-        </div>
+        <Carousel arrows autoplay>
+            {recipes.map((recipe) => {
+                if (!recipe.mainImage) {
+                    return null;
+                }
+                return (
+                    <div key={recipe.id}>
+                        <img src={`/api/image/${recipe.mainImage}`} alt={recipe.title} style={contentStyle} />
+                        <h3 style={{ lineHeight: '80px', color: '#364d79', textAlign: 'center' }}>{recipe.title}</h3>
+                    </div>
+                );
+            })}
+        </Carousel>
     );
 }
 
