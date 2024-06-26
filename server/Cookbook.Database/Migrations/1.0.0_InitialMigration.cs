5f87aad4ac4998db617563f36dbd59229ddd36f3
Linas Jakseboga <linas.jakseboga@gmail.com>
2024-06-26 22:18:53 +0200

    added database migration project

---
+++ server/Cookbook.Database/Migrations/1.0.0_InitialMigration.cs
@@ -0,0 +1,86 @@
+﻿using Cookbook.API.Entities;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using MongoDBMigrations;
+
+namespace Cookbook.Database.Migrations;
+
+internal class InitialMigration : IMigration
+{
+    public MongoDBMigrations.Version Version => new(1, 0, 0);
+
+    public string Name => "Initial Migration";
+
+    public void Down(IMongoDatabase database)
+    {
+    }
+
+    public void Up(IMongoDatabase database)
+    {
+        var collectionsNames = database.ListCollectionNames().ToList();
+
+        if (!collectionsNames.Contains("recipes"))
+        {
+            database.CreateCollection("recipes");
+        }
+
+        var recipesCollection = database.GetCollection<Recipe>("recipes");
+
+        recipesCollection.Indexes.DropAll();
+
+        var titleIndexDefinition = Builders<Recipe>.IndexKeys
+            .Text(x => x.Title)
+            .Text(x => x.Description)
+            .Text(x => x.Categories)
+            .Text("instructions.description")
+            .Text("ingredients.name");
+        recipesCollection.Indexes.CreateOne(
+            new CreateIndexModel<Recipe>(titleIndexDefinition,
+            new CreateIndexOptions
+            {
+                Name = "TextIndex"
+            }
+        ));
+
+        if (!collectionsNames.Contains("counters"))
+        {
+            database.CreateCollection("counters");
+        }
+
+        var counters = database.GetCollection<Counter>("counters");
+        var counter = counters.Find(x => x.Id == "recipe").FirstOrDefault();
+
+        if (counter == null)
+        {
+            counters.InsertOne(new Counter
+            {
+                Id = "recipe",
+                Sequence = 0
+            });
+        }
+
+        if (!collectionsNames.Contains("categories"))
+        {
+            database.CreateCollection("categories");
+        }
+
+        if (!collectionsNames.Contains("users"))
+        {
+            database.CreateCollection("users");
+        }
+
+        var usersJson = File.ReadAllText("Data/users.json");
+        var deserializedUsers = BsonSerializer.Deserialize<List<CookbookUser>>(usersJson);
+        var usersCollection = database.GetCollection<CookbookUser>("users");
+
+        foreach (var user in deserializedUsers)
+        {
+            usersCollection.ReplaceOne(
+                x => x.Id == user.Id,
+                user,
+                new ReplaceOptions() { IsUpsert = true }
+            );
+        }
+
+    }
+}
---
+++ server/Cookbook.Database/Program.cs
@@ -0,0 +1,14 @@
+﻿using MongoDBMigrations;
+using System.Reflection;
+
+var migrationEngine = new MigrationEngine()
+    .UseDatabase("mongodb://localhost:27017", "cookbook")
+    .UseAssembly(Assembly.GetExecutingAssembly())
+    .UseSchemeValidation(false);
+
+
+migrationEngine.Run();
+
+
+
+    
