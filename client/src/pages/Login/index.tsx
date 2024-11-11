4df5bb3e3daa8f90d40552d0fda88266141dd197
Linas Jakseboga <linas.jakseboga@gmail.com>
2024-11-11 21:45:01 +0100

    updated deps

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -62,7 +62,6 @@ export default function Login() {
                         <Form
                             name="basic"
                             labelCol={{ span: 8 }}
-                            // wrapperCol={{ span: 16 }}
                             style={{ maxWidth: 600 }}
                             onFinish={onFinish}
                             onFinishFailed={onFinishFailed}
